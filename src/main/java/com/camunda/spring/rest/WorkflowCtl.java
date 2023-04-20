093518fb8d007ff1c243981bc5bd28197745dfcd
nguyenthai98 <nguyenthai492018@gmail.com>
2023-04-20 21:50:13 +0700

    rest

---
+++ src/main/java/com/camunda/spring/rest/WorkflowCtl.java
@@ -0,0 +1,10 @@
+package com.camunda.spring.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/workflow")
+public class WorkflowCtl {
+    
+}
